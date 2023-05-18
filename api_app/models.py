cd3cee7ab53b5baa56acdedf2f0086d09d849f29
VicTech Electronics <victechelectronics@gmail.com>
2023-05-18 18:55:22 +0300

    Initial commit

--- api_app/models.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from main_app.models import Due
-
-# Create your models here.
-class DueSerializer(ModelSerializer):
-    class Meta:
-        model = Due
-        fields = ['customer', 'amount']
